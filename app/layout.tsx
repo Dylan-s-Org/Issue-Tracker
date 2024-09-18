21ea40bcb13ad8d237b1d90d8ca3bca89f724c42
MinHee <dylandang.dev@gmail.com>
2024-09-18 23:43:51 +0700

    dev: Build new Issue Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <NavBar />
-          <nav>{children}</nav>
+          <nav className="p-5">{children}</nav>
         </Theme>
       </body>
     </html>
