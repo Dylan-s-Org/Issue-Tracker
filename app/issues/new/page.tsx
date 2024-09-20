de5f173222028c1c77846a636fbf1b2fa9969b79
MinHee <dylandang.dev@gmail.com>
2024-09-21 01:11:49 +0700

    fix: remove index file

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,8 +9,8 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import ErrorMessage from "@/app/components/ErrorMessage/ErrorMessage";
+import Spinner from "@/app/components/Spinner/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
