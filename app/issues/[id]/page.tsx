1ea1f40a691cf8244bbc6749d9f9d0e04777548a
MinHee <dylandang.dev@gmail.com>
2024-09-30 01:07:36 +0700

    dev: Remove duplicate skeleton

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,9 @@
 import prisma from "@/prisma/client";
 import { Box, Grid, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import EditIssueButton from "../EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import EditIssueButton from "./EditIssueButton";
 
 interface Props {
   params: { id: string };
